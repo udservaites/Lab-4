2c03919590b1c167955fa91afe0bb8415d28f875
Rebecca Servaites <duchess_krs@hotmail.com>
2017-04-02 23:37:47 -0400

    commit

--- Problem 2.py
+++ Problem 2.py
@@ -7,6 +7,6 @@ called_function = ["strcpy", "sprintf", "strncpy", "wcsncpy", "swprintf"]
 segment = SegByName(".idata")
 for ref in Heads(SegStart(segment), SegEnd(segment)):
     xrefs = XrefsTo(ref, 0)
-    for refrence in xrefs:
+    for reference in xrefs:
         if Name(ref) in called_function:
-            print GetFunctionName(refrence.frm), ":", hex(reference.frm), ":", Name(ref)
+            print GetFunctionName(reference.frm), ":", hex(reference.frm), ":", Name(ref)
